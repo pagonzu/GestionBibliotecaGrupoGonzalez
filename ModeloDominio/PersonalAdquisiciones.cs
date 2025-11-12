f7e9210b11759522ec4995830ecba81ca4d8be62
dapuente <dapuente@unirioja.es>
2025-11-12 18:18:49 +0100

    Commit inicial Usuarios y Trabajadores

---
+++ ModeloDominio/PersonalAdquisiciones.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModeloDominio
+{
+    public class PersonalAdquisiciones: Trabajador
+    {
+        private int idAdquisiciones;
+
+        public PersonalAdquisiciones(String nombre, String password) : base(nombre, password) {
+            this.idAdquisiciones = 0;
+        }
+
+        public PersonalAdquisiciones(int idTrabajador, int idAdquisiciones, String nombre, String password) : base(idTrabajador, nombre, password)
+        {
+            this.idAdquisiciones = idAdquisiciones;
+        }
+
+        public int IdAdquisiciones
+        {
+            get { return idAdquisiciones; }
+        }
+    }
+}
---
+++ ModeloDominio/Usuario.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModeloDominio
+{
+    public class Usuario
+    {
+        private String dni;
+        private String nombre;
+        private Boolean baja;
+
+        public Usuario(String nombre) 
+        {
+            this.dni = "";
+            this.nombre = nombre;
+            this.baja = false;
+        }
+
+        public Usuario(String DNI, String nombre)
+        {
+            this.dni = DNI;
+            this.nombre = nombre;
+            this.baja = false;
+        }
+
+        public String DNI 
+        { 
+            get { return this.dni; }
+        }
+
+        public String Nombre
+        {
+            get { return this.nombre; }
+            set { this.nombre = value; }
+        }
+
+        public Boolean Baja
+        {
+            get { return this.baja; }
+            set { this.baja = value; }
+        }
+
+
+
+    }
+}
