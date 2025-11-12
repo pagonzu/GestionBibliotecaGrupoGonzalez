5b5f08798d32b2955196c1fe3a0195e39065cc7c
pagonzu <pagonzu@CCTL130-05>
2025-11-12 18:40:56 +0100

    prestamo y devolucion creado

--- ModeloDominio/Prestamo.cs
+++ ModeloDominio/Prestamo.cs
@@ -8,5 +8,26 @@ namespace ModeloDominio
 {
     internal class Prestamo
     {
+        private int id;
+        private DateTime fecha;
+        bool estado;
+        Trabajador trabajador;
+        Usuario usuario;
+
+        public Prestamo(int id, DateTime fecha, bool estado, Trabajador trabajador, Usuario usuario)
+        {
+            this.id = id;
+            this.fecha = fecha;
+            this.estado = estado;
+            this.trabajador = trabajador;
+            this.usuario = usuario;
+        }
+        public int getId() { return id; }
+        public DateTime getFecha() { return fecha; }
+        public bool getEstado() { return estado; }
+        public Trabajador getTrabajador() { return trabajador; }
+        public Usuario getUsuario() { return usuario; }
+        public void setFecha(DateTime fecha) { this.fecha = fecha; }
+        public void setEstado(bool estado) {  this.estado = estado; }
     }
 }
