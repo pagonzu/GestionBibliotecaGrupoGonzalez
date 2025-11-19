dac5f8b4a936b0a39b5306fb109c9e4cb808a0f1
Yazid <yaaissou@unirioja.es>
2025-11-19 18:21:58 +0100

    drghe

--- Persistencia/EjemplarDato.cs
+++ Persistencia/EjemplarDato.cs
@@ -3,16 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Persistencia
 {
     internal class EjemplarDato: Entity<String>
     {
-        private int ejemplo;
+        private bool estado;
+        private String id_Trabajador;
+        private String isbn;
 
-        public EjemplarDato(String codigo, int ejemplo): base(codigo)
+        public EjemplarDato(String codigo, bool estado,String id_t,String ISBN): base(codigo)
         {
-            this.ejemplo = ejemplo;
+            this.estado = estado;
+            this.id_Trabajador = id_t;
+            this.isbn = ISBN;
+        }
+        public bool Estado
+        {
+            get { return estado; }
+            set { estado = value; }
+        }
+
+
+        public String Trabajador
+        {
+            get { return id_Trabajador; }
+            set { id_Trabajador = value; }
+        }
+
+        public String ISBN
+        {
+            get { return isbn; }
+            set { isbn = value; }
         }
     }
 }
