cee96f365928185d8c6fcb55c24b03bfd30dca9f
dapuente <dapuente@CCTL130-18>
2025-11-19 17:18:13 +0100

    Creacion modulo Persistencia

---
+++ Persistencia/Entity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistencia
+{
+    internal class Entity<T>
+    {
+        private T id;
+
+        public Entity(T id)
+        {
+            this.id = id;
+        }
+
+        public T Id
+        {
+            get => this.id;
+        }
+    }
+}
