5ff933723a20956675058ee86a0351fa0032a369
pagonzu <pagonzu@CCTL130-16>
2025-11-19 18:49:01 +0100

    usuariodato

--- Persistencia/PrestamoDato.cs
+++ Persistencia/PrestamoDato.cs
@@ -6,8 +6,39 @@ using System.Threading.Tasks;
 
 namespace Persistencia
 {
-    internal class PrestamoDato
+    internal class PrestamoDato : Entity<int>
     {
-        private int dato;
+        private DateTime fechaPrestamo;
+        private bool estado;
+        private int idTrabajador;
+        private String dniUsuario;
+
+        public PrestamoDato(int idPrestamo, DateTime fechaPrestamo, bool estado, int idTrabajador, string dniUsuario) : base(idPrestamo)
+        {
+            this.fechaPrestamo = fechaPrestamo;
+            this.estado = estado;
+            this.idTrabajador = idTrabajador;
+            this.dniUsuario = dniUsuario;
+        }
+        public DateTime FechaPrestamo
+        {
+            get { return fechaPrestamo; }
+            set { fechaPrestamo = value; }
+        }
+        public bool Estado
+        {
+            get { return estado; }
+            set { estado = value; }
+        }
+        public int IdTrabajador
+        {
+            get { return idTrabajador; }
+            set { idTrabajador = value; }
+        }
+        public string DNIUsuario
+        {
+            get { return dniUsuario; }
+            set { dniUsuario = value; }
+        }
     }
 }
