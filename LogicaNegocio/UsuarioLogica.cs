75c972fbd428e665e684426a1382c40fac62fc7c
Yazid <yaaissou@unirioja.es>
2025-11-26 20:03:03 +0100

    sgsg

--- LogicaNegocio/UsuarioLogica.cs
+++ LogicaNegocio/UsuarioLogica.cs
@@ -4,9 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using ModeloDominio;
+
 namespace LogicaNegocio
 {
     internal class UsuarioLogica
     {
+        public void anyadirUsuario(Usuario u)
+        {
+            Persistencia.Persistencia.CREATE(u);
+        }
     }
 }
---
+++ Persistencia/Transformers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModeloDominio;
+
+namespace Persistencia
+{
+    internal static class Transformers
+    {
+        public static Usuario UsuarioDatoAUsuario(UsuarioDato usd)
+        {
+            if (usd == null) return null;
+
+            Usuario u = new Usuario(usd.Id, usd.Nombre);
+            u.Baja = usd.Estado; // Estado = true significa dado de baja
+
+            return u;
+        }
+
+        public static UsuarioDato UsuarioAUsuarioDato(Usuario u)
+        {
+            if (u == null) return null;
+
+            UsuarioDato usd = new UsuarioDato(u.DNI, u.Nombre, u.Baja);
+
+            return usd;
+        }
+    }
+}
